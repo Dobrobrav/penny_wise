c58024650f3b3a1838ed4a88ed5e28064c3bd059
SemenK <kalyakulin.semen@gmail.com>
2025-03-29 02:16:50 +0700

    .

--- app/expenses/urls.py
+++ app/expenses/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.display_expenses_view, name='expenses'),
-    path('add-expense/', views.add_expense_view, name='add_expense'),
+    path('add-expense/', views.AddExpenseView.as_view(), name='add_expense'),
 ]
