a21589ffcfdcc9810c56da005c0797b5791e4d13
SemenK <kalyakulin.semen@gmail.com>
2025-04-04 23:15:50 +0700

    .

--- app/e2e_tests/add_expense_test.py
+++ app/e2e_tests/add_expense_test.py
@@ -25,6 +25,33 @@ def browser() -> Generator_[WebDriver]:
     browser.quit()
 
 
+def test_login(
+        browser: WebDriver,
+) -> None:
+    # user enters home page
+
+    # user clicks button to log in
+
+    # page for authentication is opened
+
+    # user enters login
+
+    # user enters password
+
+    # user presses button to submit login and password
+
+    # home page is opened
+
+    # home page contains user's login
+    ...
+
+
+def test_signup(
+        browser: WebDriver,
+) -> None:
+    ...
+
+
 def test_add_expense(
         browser: WebDriver,
 ) -> None:
