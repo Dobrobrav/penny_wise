cc42236d52c3b0e673d1a4e4acd5067f7553fdf5
SemenK <kalyakulin.semen@gmail.com>
2025-04-04 16:16:51 +0700

    .

--- app/expenses/models_tests.py
+++ app/expenses/models_tests.py
@@ -8,24 +8,11 @@ from expenses.models import Expense
 
 
 @pytest.mark.parametrize(
-    'cost',
-    [123456789.55]
-)
-@pytest.mark.django_db
-def test_add_expense_should_raise_value_error_if_cost_not_valid(cost) -> None:
-    with pytest.raises(DataError):
-        Expense.objects.create(
-            name='foo',
-            cost=cost,
-            category='baz',
-        )
-
-
-@pytest.mark.parametrize(
     'cost_in, cost_from_db',
     [
         (15.5, Decimal('15.5')),
-        (15.555, Decimal('15.56'))
+        (15.555, Decimal('15.56')),
+        ('15.555', Decimal('15.56')),
     ]
 )
 @pytest.mark.django_db
@@ -49,3 +36,17 @@ def test_should_add_expense(
     assert expense.name == name
     assert expense.cost == cost_from_db
     assert expense.category == category
+
+
+@pytest.mark.parametrize(
+    'cost',
+    [123456789.55, '15,500.55']
+)
+@pytest.mark.django_db
+def test_add_expense_should_raise_value_error_if_cost_not_valid(cost) -> None:
+    with pytest.raises((DataError, ValidationError)):
+        Expense.objects.create(
+            name='foo',
+            cost=cost,
+            category='baz',
+        )
