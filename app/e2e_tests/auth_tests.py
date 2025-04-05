aaf979011fafc1f0974442831d2a81fc04f9f7e0
SemenK <kalyakulin.semen@gmail.com>
2025-04-05 16:08:49 +0700

    .

---
+++ app/e2e_tests/auth_tests.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+def test_login(
+        browser: WebDriver,
+) -> None:
+    # user enters home page
+
+    # user clicks button to log in
+
+    # page for authentication is opened
+
+    # user enters login
+
+    # user enters password
+
+    # user presses button to submit login and password
+
+    # home page is opened
+
+    # home page contains user's login
+    ...
+
+
+def test_signup(
+        browser: WebDriver,
+) -> None:
+    ...
