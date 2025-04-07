732dbc09a16857d5732a0e6caaa96eb5b3ca3f3b
SemenK <kalyakulin.semen@gmail.com>
2025-04-07 23:46:51 +0700

    .

--- app/e2e_tests/expenses_tests.py
+++ app/e2e_tests/expenses_tests.py
@@ -24,62 +24,68 @@ def test_add_expense(
     display_expenses_button = browser.find_element(by=By.ID, value='display_expenses_button')
     _click_and_wait_for_page_update(browser, display_expenses_button)
 
-    # uses doesn't see the expenses they are going to add
+    # user doesn't see the expenses they are going to add
     _assert_page_not_contains_expense(browser, name='Potatoes', cost='150', category='vegetables')
     _assert_page_not_contains_expense(browser, name='Milk', cost='80', category='dairy')
     _assert_page_not_contains_expense(browser, name='Twix', cost='55', category='chocolate bar')
 
-    # user presses button to add an expense
+    # user presses button to add 1st expense
     add_expense_button = browser.find_element(by=By.ID, value='add_expense_button')
     _click_and_wait_for_page_update(browser, add_expense_button)
 
-    # page for creating an expense is open
+    # user sees page for adding expense
     assert browser.title == 'Add expense'
+
+    # user adds 1st expense
     _enter_expense_and_save(browser, name='Potatoes', cost='150', category='vegetables')
 
-    # page with user's expenses is open
+    # user sees expenses page
     assert browser.title == 'Expenses'
 
-    # user sees the expense
+    # user sees the 1 added expense
     _assert_page_contains_expense(browser, name='Potatoes', cost='150', category='vegetables')
     _assert_page_not_contains_expense(browser, name='Milk', cost='80', category='dairy')
     _assert_page_not_contains_expense(browser, name='Twix', cost='55', category='chocolate bar')
 
-    # user clicks button to add another expense
+    # user presses button to add 2nd expense
     add_expense_button = browser.find_element(by=By.ID, value='add_expense_button')
     _click_and_wait_for_page_update(browser, add_expense_button)
 
-    # clicking the button opens page for creating an expense.
+    # user sees page for adding expense
     assert browser.title == 'Add expense'
+
+    # user adds 2nd expense
     _enter_expense_and_save(browser, name='Milk', cost='80', category='dairy')
 
-    # page with user's expenses is open
+    # user sees expenses page
     assert browser.title == 'Expenses'
 
-    # user sees both expenses
+    # user sees the 2 added expenses
     _assert_page_contains_expense(browser, name='Potatoes', cost='150', category='vegetables')
     _assert_page_contains_expense(browser, name='Milk', cost='80', category='dairy')
     _assert_page_not_contains_expense(browser, name='Twix', cost='55', category='chocolate bar')
 
-    # user clicks button to open home page
+    # user presses button to open home page
     home_page_button = browser.find_element(by=By.ID, value='home_page_button')
     _click_and_wait_for_page_update(browser, home_page_button)
 
     # user sees home page
     assert browser.title == 'Money Waste 💸'
 
-    # user presses button to add expense
+    # user presses button to add 3rd expense
     add_expense_button = browser.find_element(by=By.ID, value='add_expense_button')
     _click_and_wait_for_page_update(browser, add_expense_button)
 
-    # page for creating an expense is open
+    # user sees page for adding expense
     assert browser.title == 'Add expense'
+
+    # user adds 3rd expense
     _enter_expense_and_save(browser, name='Twix', cost='55', category='chocolate bar')
 
     # page with user's expenses is open
     assert browser.title == 'Expenses'
 
-    # user sees the expense
+    # user sees the 3 added expense
     _assert_page_contains_expense(browser, name='Potatoes', cost='150', category='vegetables')
     _assert_page_contains_expense(browser, name='Milk', cost='80', category='dairy')
     _assert_page_contains_expense(browser, name='Twix', cost='55', category='chocolate bar')
