35a3dce9e9fa8acc16a14bae6b1873d1493eeff6
SemenK <kalyakulin.semen@gmail.com>
2025-04-19 18:54:45 +0700

    .

--- app/expenses/email_tests.py
+++ app/expenses/email_tests.py
@@ -1,9 +1,22 @@
+import dataclasses
+
+import pytest
+import structlog
+
 from .email import Email
 
+logger = structlog.get_logger(__name__)
 
-def test_email() -> None:
-    Email(
+
+def test_email_immutable() -> None:
+    email = Email(
         address='test_address',
         subject='test_subject',
         content='test_content',
     )
+
+    for field in dataclasses.fields(email):
+        with pytest.raises(dataclasses.FrozenInstanceError):
+            new_value = field.type()
+            logger.info(new_value)
+            setattr(email, field.name, new_value)
